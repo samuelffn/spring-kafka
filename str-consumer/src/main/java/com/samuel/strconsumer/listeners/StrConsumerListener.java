90968087e2e519a09930b5846f1b056885aca253
samuelffn <samuelffn@gmail.com>
2024-02-14 22:57:47 -0300

    Add comentarios de estudos

--- str-consumer/src/main/java/com/samuel/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/samuel/strconsumer/listeners/StrConsumerListener.java
@@ -20,13 +20,23 @@ public class StrConsumerListener {
      * método que foi criado em StringConsumerConfig.
      * */
     @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Receive message {}", message); // Vamos logar informando que recebemos a mensagem
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message); // Vamos logar informando que recebemos a mensagem
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message); // Vamos logar informando que recebemos a mensagem
+    }
+
+    @KafkaListener(groupId = "group-3", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message); // Vamos logar informando que recebemos a mensagem
     }
 }
 
 /**
- * *Obs.: Antes de enviar a primeira mensagem. Ao olhar os logs, podemos notar o seguinte:
+ * *Obs.1: Antes de enviar a primeira mensagem. Ao olhar os logs, podemos notar o seguinte:
  * Como nós temos apenas 1 grupo de consumidor, note que ele se registrou nas partições 0 e 1 (Teremos este log:
  * group-1: partitions assigned: [str-topic-0, str-topic-1]), ou seja, ele está consumindo mensagens das duas partições.
  *
@@ -35,4 +45,26 @@ public class StrConsumerListener {
  * - Olhe os logs nos dois microsserviços, Producer e Consumer
  * - Observe que um mostra que a mensagem foi enviada enquanto que o outro mostra que foi recebida
  * - Agora abra o kafdrop (Abre o navegador e digita: localhost:19000) e observe nossas mensagens nele.
+ * */
+
+/**
+ * *Obse.2: Sobre conseito de grupos de consumo.
+ * - Quando criamos o nosso tópico (str-topico) no Producer, nós definimos que ele teria 2 partições (0 e 1). Quando criamos um tópico,
+ * por padrão ele de ter no mínimo 1 partição. Se não definirmos a quantodade de partições, o Kafka criará 1 partição.
+ * - Aqui nessa classe, quando tínhamos apenas o método listener, ele tinha se registrado nos dois grupos (lembre do log:
+ * group-1: partitions assigned: [str-topic-0, str-topic-1]), ou seja, ele está consumindo mensagens das duas partições)
+ * - Para conseguirmos explicar, vamos renomear ele para create
+ * - Quando tivermos o método create e o log, cada um deles pegará uma partição, ou seja, o log ficará assim:
+ * group-1: partitions assigned: [str-topic-0] e group-1: partitions assigned: [str-topic-1]
+ * - Quando tivermos o método history, observe que os outros dois pegarão uma partição cada e o history ficará sem partição,
+ * observe o log: group-1: partitions assigned: []
+ * - Cuidado ao criar mais Listeners do que a quantidade de grupos existentes.
+ * - Agora mudo mude o grupo do método log para group-2
+ * - Olhe os logs:
+ *  - Obseve que nenhum método ficará sem partição
+ *  - As mensagens chagarão nos dois grupos
+ * - Agora mudo mude o grupo do método history para group-3
+ *  * - Olhe os logs:
+ *  *  - Obseve que nenhum método ficará sem partição
+ *  *  - As mensagens chagarão nos três grupos
  * */
